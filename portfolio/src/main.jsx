64c4908f0757db63d0e556c7d846cb4c89d827a3
PiShrestha <pratikstha980@gmail.com>
2024-10-28 00:48:31 -0400

    Updating with NavBar.

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+/* Lets us use fonts from google fonts */
+
+import "@fontsource/outfit"
+import "@fontsource/roboto"
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
