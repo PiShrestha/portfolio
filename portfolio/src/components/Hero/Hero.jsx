e2d76f21c68e371c1e2d77ee4fcc57e57aad86fc
PiShrestha <pratikstha980@gmail.com>
2024-10-28 02:19:28 -0400

    Added Hero as a component.

---
+++ portfolio/src/components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+    return (
+        <section className = {styles.container}>
+            <div className={styles.content}>
+                <h1 className = {styles.title}>Hi, I'm Pratik</h1>
+                <p className = {styles.description}>I am a second-year Computer Science student at UVA with a strong STEM background. 
+                </p>
+                <a href="mailto:pratikshr3stha@gmail.com" className = {styles.contactBtn}> Contact Me </a>
+            </div>
+            <img src = {getImageUrl("hero/pratik.webp")} alt="Image of Pratik" className = {styles.heroImg} />
+            <div className = {styles.topBlur} />
+            <div className = {styles.bottomBlur} />
+        </section>
+    );
+}
